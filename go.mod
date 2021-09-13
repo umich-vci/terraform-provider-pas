0fe29e61a41c756a9de93c6a81bf305d1ad33f32
Adam Robinson <adarobin@umich.edu>
2021-09-13 10:35:42 -0400

    add in client and logon

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/hashicorp/terraform-plugin-docs v0.5.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.1
+	github.com/umich-vci/gopas v0.0.0-20210913134759-acca9d4d4c34
 )
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,9 +2,12 @@ package provider
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/umich-vci/gopas"
 )
 
 func init() {
@@ -14,18 +17,45 @@ func init() {
 
 	// Customize the content of descriptions when output. For example you can add defaults on
 	// to the exported descriptions if present.
-	// schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
-	// 	desc := s.Description
-	// 	if s.Default != nil {
-	// 		desc += fmt.Sprintf(" Defaults to `%v`.", s.Default)
-	// 	}
-	// 	return strings.TrimSpace(desc)
-	// }
+	schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
+		desc := s.Description
+		if s.Default != nil {
+			desc += fmt.Sprintf(" Defaults to `%v`.", s.Default)
+		}
+		return strings.TrimSpace(desc)
+	}
 }
 
 func New(version string) func() *schema.Provider {
 	return func() *schema.Provider {
 		p := &schema.Provider{
+			Schema: map[string]*schema.Schema{
+				"username": {
+					Type:        schema.TypeString,
+					Required:    true,
+					DefaultFunc: schema.EnvDefaultFunc("PAS_USERNAME", nil),
+					Description: "This is the username to use to access the Red Hat Satellite server. This must be provided in the config or in the environment variable `PAS_USERNAME`.",
+				},
+				"password": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Sensitive:   true,
+					DefaultFunc: schema.EnvDefaultFunc("PAS_PASSWORD", nil),
+					Description: "This is the password to use to access the Red Hat Satellite server. This must be provided in the config or in the environment variable `PAS_PASSWORD`.",
+				},
+				"pas_host": {
+					Type:        schema.TypeString,
+					Required:    true,
+					DefaultFunc: schema.EnvDefaultFunc("PAS_HOST", nil),
+					Description: "This is the hostname or IP address of the CyberArk PAS server. This must be provided in the config or in the environment variable `PAS_HOST`.",
+				},
+				"auth_type": {
+					Type:        schema.TypeString,
+					Required:    true,
+					DefaultFunc: schema.EnvDefaultFunc("PAS_AUTH_TYPE", nil),
+					Description: "This is the authentication type to use with the CyberArk PAS server. This must be provided in the config or in the environment variable `PAS_AUTH_TYPE`.",
+				},
+			},
 			DataSourcesMap: map[string]*schema.Resource{
 				"scaffolding_data_source": dataSourceScaffolding(),
 			},
@@ -41,17 +71,31 @@ func New(version string) func() *schema.Provider {
 }
 
 type apiClient struct {
-	// Add whatever fields, client or connection info, etc. here
-	// you would need to setup to communicate with the upstream
-	// API.
+	Client gopas.APIClient
 }
 
 func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		// Setup a User-Agent for your API client (replace the provider name for yours):
-		// userAgent := p.UserAgent("terraform-provider-scaffolding", version)
-		// TODO: myClient.UserAgent = userAgent
+	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+		userAgent := p.UserAgent("terraform-provider-pas", version)
 
-		return &apiClient{}, nil
+		username := d.Get("username").(string)
+		password := d.Get("password").(string)
+		authType := d.Get("auth_type").(string)
+		host := d.Get("pas_host").(string)
+		concurrent := true
+
+		config := gopas.NewConfiguration()
+		config.UserAgent = userAgent
+		config.Host = host
+
+		data := *gopas.NewLogonData()
+		data.UserName = &username
+		data.Password = &password
+		data.ConcurrentSession = &concurrent
+
+		client := gopas.NewAPIClient(config)
+		client.AuthApi.AuthLogon(ctx, authType).Data(data).Execute()
+
+		return &apiClient{Client: *client}, nil
 	}
 }
